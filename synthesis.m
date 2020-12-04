c66f94d4403d17c994d75b9bd363e8a66c300194
kushagra <kuku12320@gmail.com>
2020-12-04 16:57:55 +0530

    Added code for visualizing boundaries; might choose to overlay the boundary on the image, to be done by Ansh

--- synthesis.m
+++ synthesis.m
@@ -16,6 +16,7 @@ r = 2*floor(R/B); % no. of blocks in x
 c = 2*floor(C/B); % no. of blocks in y 
 out = zeros(r*B+L, c*B+L); % output gray image
 out_color = zeros(r*B+L, c*B+L, 3); % output color image
+disp_boundary = zeros(r*B+L, c*B+L); % boundary gray image
 
 for i=1:r
     for j=1:c
@@ -35,6 +36,7 @@ for i=1:r
                 errors = errors(:,1:L);
                 [cost, dir, boundary] = dp(errors);
                 boundary = [boundary zeros(B+L, B)];
+                binary_boundary = boundary.*(1-[boundary(:,2:B+L) zeros(B+L, 1)]);
             
             elseif j==1
                 mask(1:L, :) = 1;
@@ -43,7 +45,9 @@ for i=1:r
                 errors = errors(1:L, :);
                 [cost, dir, boundary] = dp(errors.');
                 boundary = [boundary zeros(B+L, B)];
+                binary_boundary = boundary.*(1-[boundary(:,2:B+L) zeros(B+L, 1)]);                
                 boundary = boundary.';
+                binary_boundary = binary_boundary.';
             
             else
                 mask(:,1:L) = 1;
@@ -54,20 +58,24 @@ for i=1:r
                 errors_h = errors(:,1:L);
                 [cost, dir, boundary_h] = dp(errors_h);
                 boundary_h = [boundary_h zeros(B+L, B)];
+                binary_boundary_h = boundary_h.*(1-[boundary_h(:,2:B+L) zeros(B+L, 1)]);
                 
                 errors_v = errors(1:L, :);
                 [cost, dir, boundary_v] = dp(errors_v.');
                 boundary_v = [boundary_v zeros(B+L, B)];
+                binary_boundary_v = boundary_v.*(1-[boundary_v(:,2:B+L) zeros(B+L, 1)]);
                 boundary_v = boundary_v.';
+                binary_boundary_v = binary_boundary_v.';
                 
                 boundary = min(boundary_h + boundary_v, 1);
+                binary_boundary = min(binary_boundary_h + binary_boundary_v, 1);
             end
             
             
             
             out((i-1)*B+1:i*B+L, (j-1)*B+1:j*B+L) = boundary.*ref + (1-boundary).*patch;
             out_color((i-1)*B+1:i*B+L, (j-1)*B+1:j*B+L,:) = boundary.*ref_color + (1-boundary).*patch_color;
-            
+            disp_boundary((i-1)*B+1:i*B+L, (j-1)*B+1:j*B+L) = binary_boundary;
             
             
         end
@@ -76,3 +84,4 @@ end
 
 figure; imshow(in_color); truesize;
 figure; imshow(out_color); truesize;
+figure; imshow(disp_boundary, [0 1]); truesize;
